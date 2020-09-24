b50365745d9e72e62a8fe9877b0415ebe433c8b8
paulwongx <20178251+paulwongx@users.noreply.github.com>
2020-09-24 19:51:25 -0400

    budget calculator

--- python/budget.py
+++ python/budget.py
@@ -1,19 +1,21 @@
 class Category:
 
     def __init__(self, category):
-        self.category = category
+        self.category = category.title()
         self.ledger = []
 
     def deposit(self, amount, description = ''):
         self.ledger.append({
-            "amount": int(amount),
+            "amount": float(amount),
             "description": description
         })
+        print(self.ledger)
+        return self.ledger
 
     def withdraw(self, amount, description = ''):
         if self.check_funds(amount):
             self.ledger.append({
-                "amount": -int(amount),
+                "amount": -float(amount),
                 "description": description
             })
             return True
@@ -29,12 +31,12 @@ class Category:
     def transfer(self, amount, category):
         if self.check_funds(amount):
             self.ledger.append({
-                "amount": -int(amount),
+                "amount": -float(amount),
                 "description": f"Transfer to {category.category}"
             })
             # To be updated to add to other ledger
             category.ledger.append({
-                "amount": int(amount),
+                "amount": float(amount),
                 "description": f"Transfer from {self.category}"
             })
             return True
@@ -52,38 +54,109 @@ class Category:
         output = self.category.center(30,'*') + '\n'
 
         for t in self.ledger:
-            output += f"{t['description']:<15}{t['amount']:>15.2f}" + '\n'
-
-            # desc_len = min(len(t['description']),23)
-            # print(f'desc_len: {desc_len}')
-            # amount_len = min(len(str(format(t['amount'],'.2f'))),7)
-            # print(f'amount_len: {amount_len}')
-            # num_spaces = 30-desc_len-amount_len
-            # print(f'num_spaces: {num_spaces}')
-
-            # line_item = t['description'][0:23]
-            # line_item += ' ' * (30-num_spaces)
-            # line_item += format(t['amount'],'.2f')[0:7]
-            # output += line_item + '\n'
+            print(type(t['amount']))
+            output += f"{t['description'][0:23]:23}{t['amount']:>7.2f}" + '\n'
 
         output += f"Total: {self.get_balance():.2f}"
         print(output)
         return output
 
-# def create_spend_chart(categories):
+def create_spend_chart(categories):
+    data = []
+
+    for c in categories:
+        total_withdrawals = 0
+        for t in c.ledger:
+            if t['amount'] < 0:
+                total_withdrawals += t['amount'] * -1
+
+        data.append({"category": c.category, "amount_spent": total_withdrawals})
+
+    total_spent = 0
+    for d in data:
+        total_spent += d['amount_spent']
+
+    for d in data:
+        print(f"amount spent: {d['amount_spent']}")
+        print(f"total spent: {total_spent}")
+        pct_spent = d['amount_spent']/total_spent*100
+        d["percentage_spent"] = int(pct_spent)
+
+    print(data)
+
+    output = 'Percentage spent by category'+'\n'
+    for i in range(100,-1,-10):
+        output += f'{i:>3}|'
+        for d in data:
+            if d['percentage_spent'] >= i:
+                output += ' o '
+            else:
+                output += '   '
+
+        output += ' \n'
+
+    output += '    ' + '-' * len(categories)*3 + '-' + '\n'
+
+    category_names = []
+    for c in categories:
+        category_names.append(c.category)
+
+    rearr_txt = []
+    max_len = len(max(category_names, key=len))
+
+    for i in range(0,max_len):
+        for c in category_names:
+            rearr_txt.append(list(c)[i]) if len(c) > i else rearr_txt.append(' ')
+
+    row = 1
+    num_categories = len(category_names)
+    index = 0
+    while row <= max_len:
+        for i in range(0,num_categories):
+            if i==0: output += '   '
+            output += '  ' + rearr_txt[index]
+            index += 1
+            if i == num_categories-1:
+                output += '  \n'
+        row += 1
+    output = output[:-1] # remove the last \n
+
+    print(output)
+    return output
 
 
 
-# create_spend_chart(["food","entertainment"])
 
 
+food = Category("Food")
+entertainment = Category("Entertainment")
+business = Category("Business")
 
-c1 = Category('Food')
-print(c1.category)
-c1.deposit(100,'income')
-c1.withdraw(20,'cash withdrawlaaaaaaaaaa')
-print(c1.get_balance())
-str(c1)
+food.deposit(900, "deposit")
+entertainment.deposit(900, "deposit")
+business.deposit(900, "deposit")
+food.withdraw(105.55)
+entertainment.withdraw(33.40)
+business.withdraw(10.99)
+actual = create_spend_chart([business, food, entertainment])
+
+
+expected = "Percentage spent by category\n100|          \n 90|          \n 80|          \n 70|    o     \n 60|    o     \n 50|    o     \n 40|    o     \n 30|    o     \n 20|    o  o  \n 10|    o  o  \n  0| o  o  o  \n    ----------\n     B  F  E  \n     u  o  n  \n     s  o  t  \n     i  d  e  \n     n     r  \n     e     t  \n     s     a  \n     s     i  \n           n  \n           m  \n           e  \n           n  \n           t  "
+
+print("EXPECTED")
+print(expected)
+
+file1 = open('chart.txt','w')
+file1.write(repr(actual) + '\n')
+file1.write(repr(expected) + '\n')
+file1.close()
+
+# c1 = Category('Food')
+# print(c1.category)
+# c1.deposit(123456789,'cash deposit abcdefghijklmnop')
+# c1.withdraw(20,'cash withdrawlabcdefghijklmnop')
+# print(c1.get_balance())
+# str(c1)
 
 
 
